66d9076d02021350961fda7c6502d51a07fee4be
Thales <dan.console.log@gmail.com>
2022-07-19 16:10:45 +0200

    just in second case

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,71 +1,9 @@
-<<<<<<< HEAD
-import { useContext, useState } from "react"
-import { Form, Button } from "react-bootstrap"
-import { useNavigate } from 'react-router-dom'
-// import authService from './../../services/auth.services'
-
-import { MessageContext } from './../../contexts/userMessage.context'
-import { AuthContext } from "../../contexts/auth.context"
-=======
 import LoginForm from "../../components/LoginForm/LoginForm"
->>>>>>> 21b60edad0a38aa16ee2beae13e960f7e0356069
 
 const LoginPage = () => {
 
-    const [loginData, setLoginData] = useState({
-        email: '',
-        password: ''
-    })
-
-    const { setShowMessage } = useContext(MessageContext)
-    const { storeToken, authenticateUser } = useContext(AuthContext)
-    const navigate = useNavigate()
-
-    const handleInputChange = e => {
-        const { value, name } = e.target
-        setLoginData({ ...loginData, [name]: value })
-    }
-
-    const handleSubmit = e => {
-        e.preventDefault()
-
-        authService
-            .login(loginData)
-            .then(({ data }) => {
-                storeToken(data.authToken)
-                authenticateUser()
-                setShowMessage({ show: true, title: `Bienvenid@!`, text: 'Sesión iniciada correctamnete' })
-            })
-            .catch(err => console.log(err))
-    }
-
-
-    const { password, email } = loginData
-
     return (
-<<<<<<< HEAD
-
-        <Form onSubmit={handleSubmit}>
-
-
-            <Form.Group className="mb-3" controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" value={email} onChange={handleInputChange} name="email" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="password">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" value={password} onChange={handleInputChange} name="password" />
-            </Form.Group>
-
-            <div className="d-grid">
-                <Button variant="dark" type="submit">Acceder</Button>
-            </div>
-
-        </Form>
-=======
         <LoginForm />
->>>>>>> 21b60edad0a38aa16ee2beae13e960f7e0356069
     )
 }
 
