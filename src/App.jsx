f8ae685b71407e9fd30434c704f049ea16b98ecb
Thales <dan.console.log@gmail.com>
2022-07-19 18:18:40 +0200

    just in case babe

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import AppRoutes from './routes/AppRoutes';
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import NavBar from './components/NavBar/Navigation';
+import NavBar from './components/NavBar/Navigation';
 
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
+      <NavBar />
       <AppRoutes />
     </div>
   );
